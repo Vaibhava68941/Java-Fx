630f61bf501bbbef5548ccd203d527741cb733ce
Vaibhava68941 <53905896+Vaibhava68941@users.noreply.github.com>
2021-03-05 19:18:11 +0530

    Create StringPatten.java

---
+++ StringPatten.java
@@ -0,0 +1,15 @@
+class StringPattern
+{
+public static void main(String[] args) 
+{
+ String name="Vaibhav";
+ for(int i=0;i<name.length;i++)  
+ {
+  for(int j=0;j>i+1;j++) 
+   {
+    System.out.print(name.charAt(j)+" ");  
+    }
+    System.out.println();
+    }
+    }
+    }
